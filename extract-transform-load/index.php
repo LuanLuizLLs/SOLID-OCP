3ce8ba1cddcccb0ff69021051733239cc8897d4d
LuanLuizLLs <luanluiz.dev@gmail.com>
2024-05-01 12:22:23 -0300

    fix: echo text

--- extract-transform-load/index.php
+++ extract-transform-load/index.php
@@ -4,26 +4,26 @@ require __DIR__.'/vendor/autoload.php';
 
 use NoOCP\Leitor;
 
-echo '<h2>Sem o Princípio Aberto/Fechado</h2>';
+echo "<h2>Sem o Princípio Aberto/Fechado</h2>";
 
-echo '<h3>Lendo arquivo CSV</h3>';
+echo "<h3>Lendo arquivo CSV</h3>";
 
 $leitor_csv = new Leitor();
 $leitor_csv->setDiretorio(__DIR__.'/assets');
 $leitor_csv->setArquivo('dados.csv');
 
-echo '<pre>';
+echo "<pre>";
 print_r($leitor_csv->lerArquivo());
-echo '</pre>';
+echo "</pre>";
 
-echo '<hr />';
+echo "<hr />";
 
-echo '<h3>Lendo arquivo TXT</h3>';
+echo "<h3>Lendo arquivo TXT</h3>";
 
 $leitor_txt = new Leitor();
 $leitor_txt->setDiretorio(__DIR__.'/assets');
 $leitor_txt->setArquivo('dados.txt');
 
-echo '<pre>';
+echo "<pre>";
 print_r($leitor_txt->lerArquivo());
-echo '</pre>';
+echo "</pre>";
